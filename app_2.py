9e03415a845e74609d22d8c5f4d1c79142e0e091
Luciano Vasconcelos <lvgalvaofilho@gmail.com>
2024-11-12 18:39:13 -0300

    app_2 libs

--- app_2.py
+++ app_2.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # pip install beautifulsoup4
 
 def fetch_page():
     url = 'https://www.mercadolivre.com.br/apple-iphone-16-pro-1-tb-titnio-preto-distribuidor-autorizado/p/MLB1040287851#polycard_client=search-nordic&wid=MLB5054621110&sid=search&searchVariation=MLB1040287851&position=6&search_layout=stack&type=product&tracking_id=92c2ddf6-f70e-475b-b41e-fe2742459774'
